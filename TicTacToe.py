2ba023090bd4e5fc84a047285dbbf6428a22e370
Sam Eads <sam.eads@gmail.com>
2022-06-30 18:57:19 -0500

    implement TicTacToeGame class to control game flow and _setup_board class.

--- TicTacToe.py
+++ TicTacToe.py
@@ -6,6 +6,44 @@ requires tkinter version 8.6 or greater
 import tkinter as tk
 from tkinter import font
 from typing import NamedTuple
+from itertools import cycle
+
+
+class Player(NamedTuple):
+    label: str
+    color: str
+
+
+class Move(NamedTuple):
+    row: int
+    col: int
+    label: str = ""
+
+
+BOARD_SIZE = 3
+DEFAULT_PLAYERS = (
+    Player(label='X', color='blue'),
+    Player(label='O', color='green'),
+)
+
+
+class TicTacToeGame:
+    def __init__(self, players=DEFAULT_PLAYERS, board_size=BOARD_SIZE):
+        self._players = cycle(players)
+        self.board_size = board_size
+        self.current_player = next(self._players)
+        self.winner_combo = []
+        self._current_moves = []
+        self._has_winner = False
+        self._winning_combos = []
+        self._setup_board()
+
+    def _setup_board(self):
+        self._current_moves = [
+            [Move(row, col) for col in range(self.board_size)]
+            for row in range[self.board_size]
+        ]
+        self._winning_combos = self._get_winning_combos()
 
 
 class TicTacToeBoard(tk.Tk):
@@ -50,17 +88,6 @@ class TicTacToeBoard(tk.Tk):
                     pady=5,
                     sticky='nsew'
                 )
-
-
-class Player(NamedTuple):
-    label: str
-    color: str
-
-
-class Move(NamedTuple):
-    row: int
-    col: int
-    label: str = ""
 
 
 def main():
