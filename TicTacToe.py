8e7ac136ad9b8cc63fb4720cb12233dc3d1c4e90
Sam Eads <sam.eads@gmail.com>
2022-06-30 16:25:38 -0500

    Create Tic Tac Toe game window class and create status display label

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,7 +1,33 @@
 """
 Tic Tac Toe Game in Python Using Tkinter
+
+requires tkinter version 8.6 or greater
 """
+import tkinter as tk
+from tkinter import font
+
+
+class TicTacToeBoard(tk.Tk):
+    def __init__(self):
+        super().__init__(self)
+        self.title("Tic Tac Toe!")
+        self._cells = {}
+        # self._create_board_display(self)
+
+    def _create_board_display(self):
+        display_frame = tk.Frame(master=self)
+        display_frame.pack(fill=tk.X)
+        self.display = tk.Label(
+            master=display_frame,
+            text="Ready?",
+            font=font.Font(size=28, weight="bold"),
+        )
+
+
+def main():
+    board = TicTacToeBoard()
+    board.mainloop()
 
 
 if __name__ == "__main__":
-    pass
+    main()
