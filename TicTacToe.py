4df7ce609f19fbeb4baa45fe163e1900207e580e
Sam Eads <sam.eads@gmail.com>
2022-06-30 19:20:13 -0500

    implement get winning combos function

--- TicTacToe.py
+++ TicTacToe.py
@@ -45,6 +45,16 @@ class TicTacToeGame:
         ]
         self._winning_combos = self._get_winning_combos()
 
+    def _get_winning_combos(self):
+        rows = [
+            [(move.row, move.col) for move in row]
+            for row in self._current_moves
+        ]
+        columns = [list(col) for col in zip(*rows)]
+        first_diagonal = [row[i] for i, row in enumerate(rows)]
+        second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
+        return rows + columns + [first_diagonal, second_diagonal]
+
 
 class TicTacToeBoard(tk.Tk):
     def __init__(self):
